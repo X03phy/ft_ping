1114afcc8875d8f70c74dfb3909e7129f88316ea
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-04-24 15:58:10 +0200

    feat: add comments

--- src/ping/ping.c
+++ src/ping/ping.c
@@ -18,17 +18,16 @@ int ping_run(s_ping_ctx *ctx)
 {
 	int ret;
 
-	if (ping_setup(ctx) != 0) {
+	if (ping_setup(ctx) != 0)
 		return (1);
-	}
 
-	signal(SIGINT, sig_handler);
+	signal(SIGINT, sig_handler); //! can fail -> to secure AND SIGNAL NOT RECOMMENDED
 
 	print_header(ctx);
-	ret = ping_loop(ctx); /* Handle error here */
+	ret = ping_loop(ctx);
 	print_stats(ctx);
 
-	close(ctx->sockfd); /* ping_cleanup */
+	close(ctx->sockfd); /* ping_cleanup() */
 	return (ret);
 }
 
