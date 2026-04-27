ad047c2c34e16b20f4d1567b3a61522a12e41f46
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-04-27 14:43:48 +0200

    fix: function prototype

--- src/icmp/build.c
+++ src/icmp/build.c
@@ -7,33 +7,33 @@
 #include <stddef.h> // NULL, size_t
 #include <string.h> // memcpy(), memset()
 
-static void icmp_init_hdr(s_icmp_pkt *pkt, unsigned short seq);
-static void icmp_fill_data_with_time(s_icmp_pkt *pkt);
+static void icmp_init_hdr(s_icmp_hdr *hdr, unsigned short seq);
+static void icmp_fill_data_with_time(char *data);
 static unsigned short icmp_checksum(void *data, size_t len);
 
 void icmp_build(s_icmp_pkt *pkt, unsigned short seq)
 {
 	memset(pkt, 0, sizeof(*pkt));
-	icmp_init_hdr(pkt, seq);
-	icmp_fill_data_with_time(pkt);
+	icmp_init_hdr(&pkt->hdr, seq);
+	icmp_fill_data_with_time(pkt->data);
 	pkt->hdr.checksum = icmp_checksum(pkt, sizeof(*pkt));
 }
 
-static void icmp_init_hdr(s_icmp_pkt *pkt, unsigned short seq)
+static void icmp_init_hdr(s_icmp_hdr *hdr, unsigned short seq)
 {
-	pkt->hdr.type = ICMP_ECHO;
-	pkt->hdr.code = 0;
-	pkt->hdr.id = htons((unsigned short)getpid());
-	pkt->hdr.seq = htons(seq);
+	hdr->type = ICMP_ECHO;
+	hdr->code = 0;
+	hdr->id = htons((unsigned short)getpid());
+	hdr->seq = htons(seq);
 }
 
-static void icmp_fill_data_with_time(s_icmp_pkt *pkt)
+static void icmp_fill_data_with_time(char *data)
 {
 	struct timeval tv;
 
 	gettimeofday(&tv, NULL); /* Cannot fail if used properly */
-	memcpy(pkt->data, &tv, sizeof(tv));
-	memset(pkt->data + sizeof(tv), 0, DATA_SIZE - sizeof(tv));
+	memcpy(data, &tv, sizeof(tv));
+	memset(data + sizeof(tv), 0, DATA_SIZE - sizeof(tv));
 }
 
 static unsigned short icmp_checksum(void *data, size_t len) //! recheck
