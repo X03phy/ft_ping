9639bda361fb7cece4280f5e60c87be5318ee8b6
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-04-27 14:28:51 +0200

    fix: changing prototype for icmp_recv + norm

--- src/icmp/net.c
+++ src/icmp/net.c
@@ -2,36 +2,40 @@
 
 #include <netinet/in.h> // struct sockaddr_in
 #include <sys/socket.h> // socklen_t, struct sockaddr, sendto(), recvfrom()
+#include <sys/time.h> // struct timeval, gettimeofday()
 #include <sys/types.h> // ssize_t
 
 #include <stddef.h> // size_t
-#include <stdio.h> // dprintf(), perror()
+#include <stdio.h> // stderr, dprintf(), perror()
 
 int icmp_send(s_icmp_pkt *pkt, int sockfd, struct sockaddr_in *addr)
 {
 	ssize_t ret;
 
-	ret = sendto(sockfd, pkt, sizeof(*pkt), 0, (struct sockaddr *)addr, sizeof(*addr));
+	ret = sendto(sockfd, pkt, sizeof(*pkt), 0, (struct sockaddr *)addr,
+                     sizeof(*addr));
 	if (ret == -1) {
 		perror("sendto()");
 		return (1);
 	}
 
 	if ((size_t)ret != sizeof(*pkt)) {
-		dprintf(2, "partial send\n");
+		dprintf(stderr, "partial send\n"); //! Modify error message
 		return (1);
 	}
 
 	return (0);
 }
 
-int icmp_recv(size_t *out, int sockfd, char *buf, size_t len, struct sockaddr_in *from)
+int icmp_recv(size_t *out, int sockfd, char *buf, size_t len,
+              struct sockaddr_in *from, struct timeval *tv_recv)
 {
 	socklen_t fromlen;
 	ssize_t r;
 
 	fromlen = sizeof(*from);
 	r = recvfrom(sockfd, buf, len, 0, (struct sockaddr *)from, &fromlen);
+	gettimeofday(tv_recv, NULL);
 	if (r == -1) {
 		perror("recvfrom()");
 		return (1);
