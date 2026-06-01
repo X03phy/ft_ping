be8737c9dbe7d64eb5da7535f9a64d529eef1e15
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-06-01 11:25:04 +0200

    feat: cleaning setup.c

--- src/ping/setup.c
+++ src/ping/setup.c
@@ -3,61 +3,53 @@
 #include <netdb.h> // struct addrinfo, getaddrinfo(), freeaddrinfo()
 #include <netinet/in.h> // struct sockaddr_in
 #include <sys/socket.h> // AF_INET, IPPROTO_ICMP, SOCK_RAW, socket()
-#include <sys/time.h> // struct timeval, gettimeofday()
+#include <sys/time.h> // struct timeval
 #include <netinet/ip.h> // IP_TTL
 
 #include <stddef.h> // NULL
 #include <stdio.h> // perror()
 #include <string.h> // memset()
 
-static int resolve_host(struct sockaddr_in *addr, const char *host);
+static int resolve_host(struct sockaddr_in *addr, const char *hostname)
+{
+	int ret;
+	struct addrinfo hints;
+	struct addrinfo *res;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_RAW;
+	ret = getaddrinfo(hostname, NULL, &hints, &res);
+	if (ret != 0) {
+		dprintf(2, "getaddrinfo(): %s\n", gai_strerror(ret)); //TODO to change
+		return (1);
+	}
+	*addr = *(struct sockaddr_in *)res->ai_addr;
+	freeaddrinfo(res);
+	return (0);
+}
 
 int ping_setup(s_ping_ctx *ctx)
 {
 	struct timeval timeout;
 
-	if (resolve_host(&ctx->addr, ctx->host) != 0) {
+	if (resolve_host(&ctx->addr, ctx->hostname) != 0) {
 		return (1);
 	}
-
 	ctx->sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (ctx->sockfd == -1) {
 		perror("socket()");
 		return (1);
 	}
-
 	if (setsockopt(ctx->sockfd, IPPROTO_IP, IP_TTL, &ctx->ttl, sizeof(ctx->ttl)) == -1) {
 		perror("setsockopt() TTL");
 		return (1);
 	}
-
 	timeout.tv_sec  = (long)ctx->interval;
 	timeout.tv_usec = (long)((ctx->interval - timeout.tv_sec) * 1000000);
 	if (setsockopt(ctx->sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1) {
 		perror("setsockopt() RCVTIMEO");
 		return (1);
 	}
-
-	return (0);
-}
-
-static int resolve_host(struct sockaddr_in *addr, const char *host)
-{
-	struct addrinfo hints, *res;
-	int ret;
-
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_RAW;
-
-	ret = getaddrinfo(host, NULL, &hints, &res);
-	if (ret != 0) {
-		dprintf(2, "getaddrinfo(): %s\n", gai_strerror(ret)); /* To change */
-		return (1);
-	}
-
-	*addr = *(struct sockaddr_in *)res->ai_addr;
-
-	freeaddrinfo(res);
 	return (0);
 }
