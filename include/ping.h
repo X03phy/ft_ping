725b75e551c3c10eaec845530228a2b90a996167
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-06-02 17:37:13 +0200

    feat: almost the end

--- include/ping.h
+++ include/ping.h
@@ -60,34 +60,33 @@ typedef struct t_ping_ctx {
 	double rtt_sum_sq;
 } t_ping_ctx;
 
-/* Functions */
-/* error.c */
-void print_error(int errcode, const char *progname);
-void print_help(const char *progname);
-
-/* args.c */
-int parse_args(t_ping_ctx *ctx, int argc, char **argv);
-
-/* icmp/icmp.c */
+/* Prototypes */
+/* icmp/build.c */
 t_icmp_pkt icmp_build(unsigned short seq);
 
-/* icmp/icmp_io.c */
+/* icmp/net.c */
 int icmp_send(const t_ping_ctx *ctx, const t_icmp_pkt *pkt);
 int icmp_recv(t_ping_ctx *ctx, t_icmp_reply *reply);
 
-/* stats.c */
+/* ping/init.c */
+void ping_init(t_ping_ctx *ctx);
+
+/* ping/setup.c */
+int ping_setup(t_ping_ctx *ctx);
+
+/* ping/run.c */
+int ping_run(t_ping_ctx *ctx);
+
+/* ping/cleanup.c */
+void ping_cleanup(t_ping_ctx *ctx);
+
+/* utils/args.c */
+int parse_args(t_ping_ctx *ctx, int argc, char **argv);
+
+/* utils/print.c */
+void print_help(const char *progname);
 void print_header(const t_ping_ctx *ctx);
 void print_response(const t_icmp_reply *reply, double rtt);
 void print_stats(const t_ping_ctx *ctx);
-
-
-/* ping/init.c */
-void ping_cleanup(t_ping_ctx *ctx);
-void ping_init(t_ping_ctx *ctx);
-int ping_run(t_ping_ctx *ctx);
-int ping_setup(t_ping_ctx *ctx);
-
-/* time/time.c */
-double time_diff_ms(struct timeval *start, struct timeval *end);
 
 #endif
--- src/utils/args.c
+++ src/utils/args.c
@@ -1,14 +1,38 @@
 #include "ping.h"
 
+#include <string.h> // strcmp()
+
+static void parse_hostname(t_ping_ctx *ctx, char *arg)
+{
+	if (!ctx->hostname)
+		ctx->hostname = arg;
+}
+
+static void parse_flag(t_ping_ctx *ctx, char **argv, int i)
+{
+	if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-?") == 0 || strcmp(argv[i], "--help") == 0)
+		ctx->flags |= FLAG_HELP;
+	else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0)
+		ctx->flags |= FLAG_VERBOSE;
+}
+
 int parse_args(t_ping_ctx *ctx, int argc, char **argv)
 {
+	int i;
+
 	if (argc < 2) {
 		print_help(argv[0]);
 		return (1);
 	}
-
 	ctx->progname = argv[0];
-	ctx->hostname = argv[1];
+	i = 1;
+	while (argv[i]) {
+		if (argv[i][0] == '-')
+			parse_flag(ctx, argv, i);
+		else
+			parse_hostname(ctx, argv[i]);
+		i++;
+	}
 
 	return (0);
 }
