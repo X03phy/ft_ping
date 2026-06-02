d1a969c7693c7a6304b839b436e76a3b5274de2e
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-06-02 17:02:40 +0200

    feat: moving function

--- src/ping/run.c
+++ src/ping/run.c
@@ -16,6 +16,19 @@ static void sig_handler(int dummy)
 	g_pingloop = 0;
 }
 
+static double time_diff_ms(struct timeval *start, struct timeval *end)
+{
+	struct timeval tmp;
+
+	tmp.tv_sec = end->tv_sec - start->tv_sec;
+	tmp.tv_usec = end->tv_usec - start->tv_usec;
+	if (tmp.tv_usec < 0) {
+		tmp.tv_sec -= 1;
+		tmp.tv_usec += 1000000;
+	}
+	return (tmp.tv_sec * 1000.0 + tmp.tv_usec / 1000.0);
+}
+
 static int ping_once(t_ping_ctx *ctx, unsigned short seq)
 {
 	t_icmp_pkt pkt;
