b5dd45f6129e5dbf867be8b75d8450431cca9317
Elio Bonutto <elio@device-26.home>
2026-06-06 17:26:28 +0200

    fix: update print_help()

--- src/utils/print.c
+++ src/utils/print.c
@@ -16,8 +16,8 @@ void print_help(const char *progname)
 	printf("Options:\n");
 	printf("  -?, --help     Give this help list\n");
 	printf("  -c, --count    Stop after sending COUNT packets\n");
+	printf("  -i, --interval Wait INTERVAL seconds between sending each packet\n");
 	printf("  -v, --verbose  Verbose output\n");
-	printf("  -w, --timeout  Stop after TIMEOUT seconds\n");
 	printf("  -f, --flood    Flood ping (root only)\n");
 	printf("  -p, --pattern  Fill ICMP packet with given pattern (hex)\n");
 	printf("  -q, --quiet    Quiet output\n");
